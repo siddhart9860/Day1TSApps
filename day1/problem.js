92ae92dc31396c155e61887081a50a3fa153dd53
Siddharth Tathe <47735494+stathe9@users.noreply.github.com>
2020-08-15 23:44:23 +0530

    Cleanup

--- day1/problem.js
+++ day1/problem.js
@@ -1,24 +1,41 @@
-var text = "TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. the typescript helps to use OOPs pattern for front-end development.";
+var text =
+  "TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. the typescript helps to use OOPs pattern for front-end development.";
 // #1
-console.log('#1');
+console.log("#1");
 var res = "";
-for (var _i = 0, _a = text.split('.'); _i < _a.length; _i++) {
-    var line = _a[_i];
-    if (line.trim().length != 0)
-        res += line.trim().substr(0, 1).toUpperCase() + line.trim().substr(1, line.length) + '. ';
+for (var _i = 0, _a = text.split("."); _i < _a.length; _i++) {
+  var line = _a[_i];
+  if (line.trim().length != 0)
+    res +=
+      line.trim().substr(0, 1).toUpperCase() +
+      line.trim().substr(1, line.length) +
+      ". ";
 }
 console.log(res);
-// #2 
-var aOccurences = text.trim().split('').filter(function (x, y) { return x == 'a'; }).length;
-console.log("#2 No. of occurences of Character \"a\" --> " + aOccurences);
-var isOccurences = text.trim().split(' ').filter(function (x, y) { return x === 'is'; }).length;
-console.log("#2 No. of occurences of word \"is\" --> " + isOccurences);
+// #2
+var aOccurences = text
+  .trim()
+  .split("")
+  .filter(function (x, y) {
+    return x == "a";
+  }).length;
+console.log('#2 No. of occurences of Character "a" --> ' + aOccurences);
+var isOccurences = text
+  .trim()
+  .split(" ")
+  .filter(function (x, y) {
+    return x === "is";
+  }).length;
+console.log('#2 No. of occurences of word "is" --> ' + isOccurences);
 // #3
-console.log('#3');
+console.log("#3");
 var res3 = "";
-for (var _b = 0, _c = text.split(' '); _b < _c.length; _b++) {
-    var word = _c[_b];
-    if (word.trim().length != 0)
-        res3 += word.trim().substr(0, 1).toUpperCase() + word.trim().substr(1, word.length) + ' ';
+for (var _b = 0, _c = text.split(" "); _b < _c.length; _b++) {
+  var word = _c[_b];
+  if (word.trim().length != 0)
+    res3 +=
+      word.trim().substr(0, 1).toUpperCase() +
+      word.trim().substr(1, word.length) +
+      " ";
 }
 console.log(res3);
--- day1/problem.ts
+++ day1/problem.ts
@@ -1,43 +1,54 @@
-let text = "TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. the typescript helps to use OOPs pattern for front-end development.";
+let text =
+  "TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. the typescript helps to use OOPs pattern for front-end development.";
 
 // #1
-console.log('#1');
+console.log("#1");
 
 let res = "";
 
-for (let line of text.split('.')) {
-
-    if (line.trim().length != 0)
-        res += line.trim().substr(0, 1).toUpperCase() + line.trim().substr(1, line.length) + '. ';
+for (let line of text.split(".")) {
+  if (line.trim().length != 0)
+    res +=
+      line.trim().substr(0, 1).toUpperCase() +
+      line.trim().substr(1, line.length) +
+      ". ";
 }
 
 console.log(res);
 
-// #2 
-let aOccurences = text.trim().split('').filter((x, y) => x == 'a').length;
+// #2
+let aOccurences = text
+  .trim()
+  .split("")
+  .filter((x, y) => x == "a").length;
 
 console.log(`#2 No. of occurences of Character "a" --> ${aOccurences}`);
 
-let isOccurences = text.trim().split(' ').filter((x, y) => x === 'is').length;
+let isOccurences = text
+  .trim()
+  .split(" ")
+  .filter((x, y) => x === "is").length;
 
 console.log(`#2 No. of occurences of word "is" --> ${isOccurences}`);
 
 // #3
-console.log('#3');
+console.log("#3");
 let res3 = "";
 
-for (let word of text.split(' ')) {
-
-    if (word.trim().length != 0)
-        res3 += word.trim().substr(0, 1).toUpperCase() + word.trim().substr(1, word.length) + ' ';
+for (let word of text.split(" ")) {
+  if (word.trim().length != 0)
+    res3 +=
+      word.trim().substr(0, 1).toUpperCase() +
+      word.trim().substr(1, word.length) +
+      " ";
 }
 
 console.log(res3);
 
 // Output:
-    // #1
-    // TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. The typescript helps to use OOPs patternern for front-end development.
-    // #2 No. of occurences of Character "a" --> 12
-    // #2 No. of occurences of word "is" --> 2
-    // #3
-    // TypeScrtipt Is A Front-end Language That Is An Alternative To JavaSCript. This Improves The JavaScript Productivity. The Typescript Helps To Use OOPs Patternern For Front-end Development.
+// #1
+// TypeScrtipt is a front-end language that is an alternative to JavaSCript. This improves the JavaScript productivity. The typescript helps to use OOPs patternern for front-end development.
+// #2 No. of occurences of Character "a" --> 12
+// #2 No. of occurences of word "is" --> 2
+// #3
+// TypeScrtipt Is A Front-end Language That Is An Alternative To JavaSCript. This Improves The JavaScript Productivity. The Typescript Helps To Use OOPs Patternern For Front-end Development.
